a458f9ef0370c5abe5259d522be36f90a2262121
Filipe Cruz <filipe.cruz.ribeiro@gmail.com>
2021-01-17 06:47:38 -0300

    fix: margin, images and changing email

--- src/sections/about.js
+++ src/sections/about.js
@@ -33,14 +33,14 @@ const About = ({ contentModuleId }) => {
                     }
                 }
             }
-        }
+        } 
     `);
 
     const content = data.allContentfulLayoutAbout.edges.find(edge => edge.node.id === contentModuleId); 
 
     return (
         <section id="about" className="about-us bg-gray">
-            <div className="container section mx-auto">
+            <div className="hero container section mx-auto">
                 <div className="about-us__content">
                     <h2 className="section__title" data-sal="fade" data-sal-easing="ease-in-cubic">{ content.node.heading }</h2>
                     <p data-sal="slide-up" data-sal-easing="ease-in-cubic" data-sal-delay="100">{ content.node.description.description }</p>
